b99ae5e188596405641e74365e2e30ca6439d7a2
stefan-bouognong <stefanbouognong@gmail.com>
2026-03-20 09:00:36 +0100

    mise a jour du setting pour prendre en compte la configuration de neon db

--- backend/settings.py
+++ backend/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-
+import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Charger .env
@@ -91,17 +91,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # ======================
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST", '127.0.0.1'),
-        'PORT': os.getenv("DB_PORT", '3306'),
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 # ======================
 # PASSWORD VALIDATION
@@ -135,4 +129,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS (LOCAL DEV)
 # ======================
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
+
+CORS_ALLOWED_ORIGINS = [
+    "https://ton-frontend.onrender.com",
+]
